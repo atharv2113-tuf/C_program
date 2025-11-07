6a015165d109caef528f054f60dc463005759193
atharv kulkarni <atharv.kulkarni1308@gmail.com>
2025-11-07 22:07:04 +0530

    Add files via upload

---
+++ divide2numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main (){
+    int num1,num2,answer;
+
+    printf("please enter two numbers.");
+    scanf("%d %d",&num1,&num2);
+
+    answer = num1/num2;
+            printf("answer=%d",answer);
+
+            return 0;
+
+
+
+
+}
---
+++ multiply2numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int num1,num2,answer;
+    printf("please enter two numbers.");
+    scanf("%d %d",&num1,&num2);
+
+    answer = num1 * num2;
+        printf("answer=%d\n",answer);
+
+    return 0;
+
+}
