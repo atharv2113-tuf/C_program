f6728bd0c3530d7b36dbeaa62ff5bb4b6ae2d1dc
atharv kulkarni <atharv.kulkarni1308@gmail.com>
2025-11-08 10:08:52 +0530

    Add files via upload

---
+++ Sareaofcube.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float a, area;
+         printf("Enter side: ");
+         scanf("%f", &a);
+    area = 6 * a * a;
+             printf("Surface Area of Cube = %.2f", area);
+    return 0;
+}
